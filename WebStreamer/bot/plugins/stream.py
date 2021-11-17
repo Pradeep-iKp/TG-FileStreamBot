e98f059930a2e10e671542af36381b4a8f293061
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2021-11-18 01:05:34 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -102,7 +102,7 @@ async def private_receive_handler(c: Client, m: Message):
             parse_mode="HTML", 
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Download Now", url=stream_link),
-                                                InlineKeyboardButton("Delete", callback_data=f"delete+{message_id}")
+                                                InlineKeyboardButton("Delete", callback_data=f"delete+{log_msg.message_id}")
                                               ]]),
             quote=True
         )
